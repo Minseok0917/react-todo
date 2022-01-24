5090023c9f3e90a426b7ca61f1a49068d2523d7a
Minseok0917 <nminseok0917@naver.com>
2022-01-24 17:54:47 +0900

    getIdx 추가
    기존 length 로 idx 를 추가한 경우에는 나중에 수정이나 삭제할 경우 다중으로 삭제될 경우가 있기에 고유값으로 늘려주기로 수정했음

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -6,25 +6,29 @@ import TodoList from './TodoList';
 
 export default function(){
 	const [state,setState] = useState({
-		todo:[]
+		todos:[]
 	});
 
 	const isValidTodo = (tname) => {
-		const isName = state.todo.some( ({name}) => name === tname );
+		const isName = state.todos.some( ({name}) => name === tname );
 		const isLength = tname.length === 0;
 		return (isName || isLength);
 	};
+	const getIdx = (() => {
+		let idx = 0;
+		return () => idx++;
+	})()
 
 	const todoCore = {
 		addTodo:function(addItem){
 			if( isValidTodo(addItem.name) ) return false;
-			addItem.idx = state.todo.length;
-			state.todo = [...state.todo,addItem];
+			addItem.idx = getIdx();
+			state.todos = [...state.todos,addItem];
 			setState(state);
 			return true;
 		},
 		removeTodo:function(removeItem){
-			state.todo = state.todo.filter( item => removeItem !== item );
+			state.todos = state.todos.filter( item => removeItem !== item );
 			setState(state);
 		},
 	}
@@ -32,7 +36,7 @@ export default function(){
 	return (
 		<div className="todo">
 			<AddInput addTodo={todoCore.addTodo} />
-			<TodoList todos={state.todo} />
+			<TodoList todos={state.todos} />
 		</div>
 	);
 }
