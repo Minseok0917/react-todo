061db4f81d983e4feee4c01d0d8df1f52e275a57
Minseok0917 <nminseok0917@naver.com>
2022-01-25 17:15:34 +0900

    calendar 렌더링 생성

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -1,2 +1,56 @@
 import { useState } from 'react';
-import utils from './utils';
+import calendarPackage,{ 
+	getDate,
+	getNextMonth,
+	getPrevMonth
+} from './utils';
+
+const maxDayCount = 42;
+const prevMonthBlock = ({monthFirstDayOfWeekNumber,prevMonthLastDay}) => {
+	// prevMonthLastDay-monthFirstDayOfWeekNumber+idx+1
+	return Array(monthFirstDayOfWeekNumber).fill().map( (_,idx) => (
+		<p>{prevMonthLastDay-idx}</p>
+	)).reverse();
+};
+const currentMonthBlock = ({monthLastDay}) => {
+	return Array(monthLastDay).fill().map((_,idx)=>(
+		<p>{idx+1}</p>
+	));
+};
+const nextMonthBlock = ({monthFirstDayOfWeekNumber,prevMonthLastDay}) => {
+	const nextDayCount = maxDayCount-monthFirstDayOfWeekNumber-prevMonthLastDay;
+	return Array(nextDayCount).fill().map((_,idx)=>(
+		<p>{idx+1}</p>
+	));
+}
+
+
+
+ export default function(){
+ 	// variable
+ 	const today = getDate();
+ 	const [state,setState] = useState({
+ 		...calendarPackage(today)
+ 	});
+ 	const { selectDate } = state;
+
+ 	
+ 	// event methods
+ 	const nextMonthHandle = () => setState({
+ 		...calendarPackage(getNextMonth(selectDate))
+ 	});
+ 	const prevMonthHandle = () => setState({
+ 		...calendarPackage(getPrevMonth(selectDate))
+ 	});
+
+
+ 	return(
+ 		<div className="calendar">
+ 			{state.selectDate}
+		 	{prevMonthBlock(state)}
+		 	{currentMonthBlock(state)}
+		 	{nextMonthBlock(state)}
+ 		</div>
+ 	);
+ }
+
