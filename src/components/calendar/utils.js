ae84fd2bb0300f4bf32db700733da1abbccca886
Minseok0917 <nminseok0917@naver.com>
2022-01-25 16:47:25 +0900

    calendar utils method 추가

--- src/components/calendar/utils.js
+++ src/components/calendar/utils.js
@@ -1,26 +1,47 @@
 import dayjs from 'dayjs';
 
 
-export const today = function(){
-	return dayjs().format('YYYY-MM-DD');
+export const getDate = function(dateValue){
+	return dayjs(dateValue).format('YYYY-MM-DD');
 }
 
-export const monthFirstDayOfWeekNumber = function(date){
-	const monthFirstDay =  new Date(date);
+export const monthFirstDayOfWeekNumber = function(dateValue){
+	const monthFirstDay =  new Date(dateValue);
 	monthFirstDay.setDate(1);
 	return monthFirstDay.getDay();
 }
 
-export const monthLastDay = function(date){
-	const monthLastDay =  new Date(date);
+export const monthLastDay = function(dateValue){
+	const monthLastDay =  new Date(dateValue);
 	monthLastDay.setMonth(monthLastDay.getMonth()+1);
 	monthLastDay.setDate(0);
 	return monthLastDay.getDate();
 }
 
-
-export default {
-	today,
-	monthFirstDayOfWeekNumber,
-	monthLastDay
+export const nextMonth = function(dateValue){
+	const month = new Date(dateValue);
+	month.setMonth(month.getMonth()+1);
+	return month;
 }
+
+export const prevMonth = function(dateValue){
+	const month = new Date(dateValue);
+	month.setMonth(month.getMonth()-1);
+	return month;
+}
+
+export const YYYMMDD = function(dateValue){
+	const date = new Date(dateValue)
+	return {
+		year:date.getFullYear(),
+		month:date.getMonth()+1,
+		day:date.getDate()
+	};
+}
+
+export default (dateValue)=>({
+	...YYYMMDD(dateValue),
+	selectDate:getDate(dateValue),
+	monthFirstDayOfWeekNumber:monthFirstDayOfWeekNumber(dateValue),
+	monthLastDay:monthLastDay(dateValue)
+})
