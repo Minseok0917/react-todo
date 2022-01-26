3cc932fcb5a4859a3dd67178ddc54e80b3240476
Minseok0917 <nminseok0917@naver.com>
2022-01-26 19:21:27 +0900

    nav css 연동 & css 스타일링 & 페이지 Link 추가

--- src/components/template/nav/index.js
+++ src/components/template/nav/index.js
@@ -1,7 +1,26 @@
+import './index.css';
+import { Link } from "react-router-dom";
+
+
 export default function(){
 	return(
 		<nav>
-			Nav
+			<div className="logo-block">
+				<h1>
+					TD
+				</h1>
+			</div>
+			<div className="menu-block">
+				<div className="item">
+					<Link to="/">홈</Link>					
+				</div>
+				<div className="item">
+					<Link to="/calendar">달력</Link>
+				</div>
+				<div className="item">
+					<Link to="/statistic">통계</Link>
+				</div>
+			</div>
 		</nav>
 	);
 }
