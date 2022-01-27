e2141d0ed9d7124a87fc5e8d3e8b095baae92d5e
Minseok0917 <nminseok0917@naver.com>
2022-01-27 19:25:11 +0900

    todo page upate

--- src/views/index.js
+++ src/views/index.js
@@ -2,3 +2,4 @@ import './index.css';
 export { default as Home }  from './home';
 export { default as Calendar }  from './calendar';
 export { default as Statistic }  from './statistic';
+export { default as Todo } from './todo';
