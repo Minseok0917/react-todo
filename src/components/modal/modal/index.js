8785f533e308fc3d32a1052959993ae45d87ccba
Minseok0917 <nminseok0917@naver.com>
2022-01-29 22:13:26 +0900

    modal outsideClick 추가

--- src/components/modal/modal/index.js
+++ src/components/modal/modal/index.js
@@ -1,8 +1,13 @@
+import { createRef } from 'react';
 import './index.css';
 
-export default function({active,closeHandle,child={}}){
+export default function({active,closeHandle,child={},close}){
+	const $modal = createRef();
+	const selfClose= function(e){
+		if ( $modal.current === e.target  ) close();
+	}
 	return (
-		<div className="modal">
+		<div className="modal"  ref={$modal} onClick={selfClose}>
 			<div className="modal-container">
 				{[child.header].map( (Child,idx) => <Child key={idx} /> )}
 				{[child.section].map( (Child,idx) => <Child key={idx} /> )}
