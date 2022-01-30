3d3d315b52fb4750a0348e0dcaa5705417e6ce5b
Minseok0917 <nminseok0917@naver.com>
2022-01-30 21:46:20 +0900

    간단한 드래그 기능 추가
    나중에 수정이 필요해 보임

--- src/components/todolist/index.js
+++ src/components/todolist/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ModalCreateTodo from '../modal/createTodo';
 import ModalDetailTodo from '../modal/detailTodo';
 import './index.css';
@@ -7,11 +7,17 @@ import {
 	setStorage
 } from './utils';
 
-let selectTodo;
+let selectTodo = {};
+let focusTodo = {};
+let selectStatus = '';
+let isDown = false;
+let isDrag = false;
+let downPos = {};
+let movePos = {};
 
 
 export default function({date}){
-	const loadTodo = getStorage(date);
+	const loadTodo = getStorage(date).sort((a,b) => a.idx - b.idx);
 	const [modalState,setModalState] = useState({
 		createTodo:false,
 		detailTodo:false,
@@ -22,13 +28,77 @@ export default function({date}){
 			[key]:status
 		});
 	};
-	const modalClose = function(){
-		modalController('createTodo',false);
+
+	const globalMouseMove = function(e){
+		if( !isDown ) return;
+		isDrag = true;
+		movePos.x = e.pageX;
+		movePos.y = e.pageY;
+	};
+	const globalMouseUp = function(){
+		if( !isDrag || !isDown || Object.keys(modalState).some( isModal => isModal ) ) return;
+
+		if( selectStatus.length ){
+			selectTodo.status = selectStatus;
+		}
+
+		selectTodo = {};
+		focusTodo = {};
+		selectStatus = '';
+		isDown = false;
+		isDrag = false;
+		downPos = {};
+		movePos = {};
+
+		setTodoState([...todoState]);
+		setStorage(date,[...todoState]);
+	}
+	useEffect(()=>{ // mount 
+		document.addEventListener('mousemove',globalMouseMove);
+		document.addEventListener('mouseup',globalMouseUp);
+
+		return(()=>{  // unmount 
+			document.removeEventListener('mousemove',globalMouseMove);
+			document.removeEventListener('mouseup',globalMouseUp);
+		});
+	});
+
+	const modalClose = function(key){
+		modalController(key,false);
 	};
 
+	const getIdx = (() => {
+		let count = (todoState.slice(-1)[0]?.idx ?? -1)+1;
+		return () => count++;
+	})();
+
 	const addTodo = function(todoValue){
+		todoValue.idx = getIdx();
+		todoValue.pIdx = todoValue.idx;
+		todoValue.status = 'todo';
 		setTodoState([...todoState,todoValue]);
 		setStorage(date,[...todoState,todoValue]);
+	};
+	const mousedownHandle = function(e,todoItem){
+		selectTodo = todoItem;
+		downPos.x = e.pageX;
+		downPos.y = e.pageY;
+		isDown = true;
+	};
+
+
+	const mouseEnterHandle = function(e,todoItem){
+		focusTodo = todoItem;
+	}
+	const mouseleaveHandle = function(e){
+		focusTodo = {};
+	}
+
+	const statusMouseEnterHandle = function(statusName){
+		selectStatus = statusName;
+	};
+	const statusMouseLeaveHandle = function(){
+		selectStatus = '';
 	};
 
 
@@ -40,28 +110,66 @@ export default function({date}){
 				<button className="btn" onClick={ () => modalController('createTodo',true) }>일단 추가</button>
 			</div>
 			<div className="todo-container">
-				<div className="todo-area todo-block">
+				<div className="todo-area todo-block" 
+					onMouseEnter={()=> statusMouseEnterHandle('todo') }
+					onMouseLeave={()=> statusMouseLeaveHandle() }>
 					<h3 className="title">할 일</h3>
 					<div className='item-container'>
-						{ todoState.map( ({title,description},idx) => (
-							<div className="item" key={idx} onClick={ ()=> {
+						{ todoState.filter( ({status}) => status === 'todo' ).map( (todoItem,idx) => (
+							<div className="item" key={idx} 
+							onMouseDown={(e)=>mousedownHandle(e,todoItem)} 
+							onMouseEnter={(e)=>mouseEnterHandle(e,todoItem)} 
+							onMouseLeave={(e)=>mouseleaveHandle(e)} 
+							onDoubleClick={ ()=> {
 								modalController('detailTodo',true);
-								selectTodo = {title,description}
+								selectTodo = todoItem;
 							} }>
-								{title}
+								{todoItem.title}
 							</div>
 						))}
 					</div>
 				</div>
-				<div className="todo-area processing-block">
+				<div className="todo-area processing-block" 
+					onMouseEnter={()=> statusMouseEnterHandle('process') }
+					onMouseLeave={()=> statusMouseLeaveHandle() }>
 					<h3 className="title">진행중</h3>
+					<div className='item-container'>
+						{ todoState.filter( ({status}) => status === 'process' ).map( (todoItem,idx) => (
+							<div className="item" key={idx} 
+							onMouseDown={(e)=>mousedownHandle(e,todoItem)} 
+							onMouseEnter={(e)=>mouseEnterHandle(e,todoItem)} 
+							onMouseLeave={(e)=>mouseleaveHandle(e)} 
+							onDoubleClick={ ()=> {
+								modalController('detailTodo',true);
+								selectTodo = todoItem;
+							} }>
+								{todoItem.title}
+							</div>
+						))}
+					</div>
 				</div>
-				<div className="todo-area close-block">
+				<div className="todo-area close-block" 
+					onMouseEnter={()=> statusMouseEnterHandle('close') }
+					onMouseLeave={()=> statusMouseLeaveHandle() }>
 					<h3 className="title">완료</h3>
+					<div className='item-container'>
+						{ todoState.filter( ({status}) => status === 'close' ).map( (todoItem,idx) => (
+							<div className="item" key={idx} 
+							onMouseDown={(e)=>mousedownHandle(e,todoItem)} 
+							onMouseEnter={(e)=>mouseEnterHandle(e,todoItem)} 
+							onMouseLeave={(e)=>mouseleaveHandle(e)} 
+							onDoubleClick={ ()=> {
+								modalController('detailTodo',true);
+								selectTodo = todoItem;
+							} }>
+								{todoItem.title}
+							</div>
+						))}
+					</div>
 				</div>
 			</div>
-			{  modalState.createTodo && <ModalCreateTodo addTodo={addTodo} close={modalClose} /> }
-			{  modalState.detailTodo && <ModalDetailTodo todoItem={selectTodo} close={modalClose} /> }
+			{  modalState.createTodo && <ModalCreateTodo addTodo={addTodo} close={ ()=> modalClose('createTodo')} /> }
+			{  modalState.detailTodo && <ModalDetailTodo todoItem={selectTodo} close={ ()=> modalClose('detailTodo')} /> }
 		</div>
 	);
 }
