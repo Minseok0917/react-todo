85549900e1a9b6099904516702e1aabdf319ce84
Minseok0917 <nminseok0917@naver.com>
2022-01-31 18:34:53 +0900

    header icon 추가 및 레이아웃 변경

--- src/components/template/header/index.js
+++ src/components/template/header/index.js
@@ -1,9 +1,17 @@
 import './index.css';
+import {
+	FaGithub
+} from 'react-icons/fa';
 
 export default function(){
 	return(
 		<header>
-			TIL 주소랑 Github 링크 박아둘꺼임
+			<div className="container">
+				<a href="https://minseok0917.github.io/TIL/" target="_blank">TIL</a>
+				<a href="https://github.com/Minseok0917" target="_blank">
+					<FaGithub size="32px" fill="#000" />
+				</a>
+			</div>
 		</header>
 	);
 }
